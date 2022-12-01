5d044b04d3ea56d843018554002d24ed9feed651
Juan Leiva <juan.leiva@agileengine.com>
2022-12-01 11:45:38 -0300

    Implemented new sorting method

--- main.js
+++ main.js
@@ -2,17 +2,35 @@ const { performance } = require("perf_hooks");
 const bubbleSort = require("./bubbleSort");
 const insertionSort = require("./insertionSort");
 const selectionSort = require("./selectionSort");
+const improvedBubbleSort = require("./improvedBubbleSort");
 const generateList = require("./utils");
+const savedList = require("./list");
 
-function main(sortingMethod, size, bestCase = false, print = true) {
-  if (!["bubble", "insertion", "selection", "quick"].includes(sortingMethod)) {
+function main(
+  sortingMethod,
+  size,
+  saveList = false,
+  setList = false,
+  bestCase = false
+) {
+  if (
+    !["bubble", "insertion", "selection", "improvedBubble"].includes(
+      sortingMethod
+    )
+  ) {
     throw new Error("Esse método de ordenação não foi implementado");
   }
   if (size > 100000 || size <= 0) {
     throw new Error("Enter a valid list size (from 1 to 100k)");
   }
-  let list = generateList(size, bestCase);
-  console.log(`Método de Ordenação Selecionado: ${sortingMethod}`);
+  if (setList) {
+    var list = savedList;
+  } else {
+    var list = generateList(size, bestCase, saveList);
+  }
+  console.log(
+    `********** Método de Ordenação Selecionado: ${sortingMethod} **********`
+  );
   console.log("Lista inicial:");
   console.log(list);
   let startTime = performance.now();
@@ -21,6 +39,9 @@ function main(sortingMethod, size, bestCase = false, print = true) {
     case "bubble":
       result = bubbleSort(list);
       break;
+    case "improvedBubble":
+      result = improvedBubbleSort(list);
+      break;
     case "insertion":
       result = insertionSort(list);
       break;
@@ -28,13 +49,14 @@ function main(sortingMethod, size, bestCase = false, print = true) {
       result = selectionSort(list);
       break;
   }
-  if (print) {
-    console.log(result.sorted);
-  }
+  console.log("Sorted list:");
+  console.log(result.sorted);
   console.log(`Tempo de execução: ${performance.now() - startTime}`);
   console.log(`Número de comparações: ${result.comparisonCount}`);
   console.log(`Número de trocas: ${result.swapCount}`);
   return;
 }
-
-main("selection", 900, true);
+const tamanho = 100000;
+console.log("********** Execução Iniciada **********");
+console.log(`********** Tamanho: ${tamanho} **********`);
+main("selection", tamanho, false, true);
